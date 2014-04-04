32d9b2c9c686e8d3427c75d09a649ab7b439a0ce
Alexandre Bury <alexandre.bury@gmail.com>
2014-04-04 21:08:50 +0200

    Second stub

--- src/com/hashcoders/noexception/HashCodeTrial.java
+++ src/com/hashcoders/noexception/HashCodeTrial.java
@@ -1,9 +1,17 @@
 package com.hashcoders.noexception;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 
 public class HashCodeTrial {
 
-	public class Board {
+	public static class Board {
 		boolean[][] cells;
 		int w,h;
 		
@@ -14,25 +22,51 @@ public class HashCodeTrial {
 		}
 	}
 	
-	public static Board loadBoard(String filename) {
+	public static Board loadBoard(String filename) throws IOException {
+		
 		//TODO : make it work
-	    
-		return null;
+		BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+		String line;
+		// First: size
+		line = br.readLine();
+		String[] size = line.split(" ");
+		int w = Integer.parseInt(size[1]);
+		int h = Integer.parseInt(size[0]);
+		Board board = new Board(w, h);
+		
+		int j = 0;
+		while ((line = br.readLine()) != null) {
+		   for (int i = 0; i < line.length(); i++)
+			   board.cells[i][j] = (line.charAt(i) == '#');
+		   j++;
+		}
+		br.close();
+		
+		return board;
 	}
 	
-	public static void naiveSolution(Board board) {
+	public static void naiveSolution(Board board, String output) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter(output, "UTF-8");
+		
 		for (int w = 0; w < board.w; w++)
 			for (int h = 0; h < board.h; h++)
 				if (board.cells[w][h])
-					System.out.println("PAINTSQ " + w + " " + h + " 0");
+					writer.println("PAINTSQ " + w + " " + h + " 0");
 		
+		writer.close();
 	}
 	
 	public static void main(String[] args) {
 		String input = "doodle.txt";
-		Board board = loadBoard(input);
 		
-		naiveSolution(board);
+		try {
+			Board board = loadBoard(input);
+			naiveSolution(board, "output.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
